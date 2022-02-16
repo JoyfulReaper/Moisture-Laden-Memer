1c6693820388ff109d2a72c63596410b8a70c565
Kyle Givler <xvegan88x@gmail.com>
2022-02-16 17:35:23 -0500

    Use commands in files

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-const { Client, Intents } = require('discord.js');
+const fs = require('fs');
+const { Client, Collection, Intents } = require('discord.js');
 const { token } = require('./config.json');
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+client.commands = new Collection();
+const commandFiles = fs.readdirSync('./commands')
+    .filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.data.name, command);
+}
 
 client.once('ready', () => {
     console.log('Ready!');
@@ -10,18 +20,17 @@ client.once('ready', () => {
 client.on('interactionCreate', async interaction => {
     if (!interaction.isCommand()) return;
 
-    const { commandName } = interaction;
+    const command = client.commands.get(interaction.commandName);
 
-    if (commandName === 'ping') {
-        await interaction.reply('Pong!');
-    }
-     else if (commandName === 'server') {
-        await interaction.reply(`Server name: ${interaction.guild.name}\n`
-            + `Total members: ${interaction.guild.memberCount}\n`
-            + `Created: ${interaction.guild.createdAt}`);
-    }
-     else if (commandName === 'user') {
-        await interaction.reply(`Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`);
+    if(!command)
+        return;
+
+    try {
+        await command.execute(interaction);
+    } catch (error)
+    {
+        console.error(error);
+        await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true});
     }
 });
 
