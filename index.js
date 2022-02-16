c8c8ff024081c877852dd0345a16b8402489b6a2
Kyle Givler <xvegan88x@gmail.com>
2022-02-15 21:34:38 -0500

    testing stuffs

---
+++ index.js
@@ -0,0 +1,27 @@
+const { Client, Intents } = require('discord.js');
+const { token } = require('./config.json');
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+client.once('ready', () => {
+    console.log('Ready!');
+});
+
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isCommand()) return;
+
+    const { commandName } = interaction;
+
+    if (commandName === 'ping') {
+        await interaction.reply('Pong!');
+    }
+     else if (commandName === 'server') {
+        await interaction.reply(`Server name: ${interaction.guild.name}\n`
+            + `Total members: ${interaction.guild.memberCount}`);
+    }
+     else if (commandName === 'user') {
+        await interaction.reply('User info!');
+    }
+});
+
+client.login(token);
