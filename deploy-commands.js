f940977aba64e3fca8c2497dc1009663ca18a060
Kyle Givler <xvegan88x@gmail.com>
2022-02-16 17:45:17 -0500

    Update deploy script

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,8 +1,16 @@
+// Run this file to register slash commands
+// Set registerGlobal to true to register for all guilds (can take and hour)
+
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
 const { clientId, guildId, token } = require('./config.json');
 
+// Change this to true to make slash commands globally avaliable instead of only
+// in guildId
+const registerGlobal = false;
+
+// The slash commands to register
 const commands = [
     new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!'),
     new SlashCommandBuilder().setName('server').setDescription('Replies server info!'),
@@ -12,6 +20,13 @@ const commands = [
 
 const rest = new REST({ version: '9' }).setToken(token);
 
-rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+if (registerGlobal) {
+     rest.put(Routes.applicationCommands(clientId), { body: commands })
+     .then(() => console.log('Successfully registered application commands. Please wait one hour to take effect.'))
+     .catch(console.error);
+}
+else {
+    rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
     .then(() => console.log('Successfully registered application commands.'))
     .catch(console.error);
+}
